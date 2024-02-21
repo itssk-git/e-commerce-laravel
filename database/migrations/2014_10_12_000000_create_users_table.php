9c39d2c8ae0995cc2891d2939fd6b1d8c2b5b269
Sachin Karki <chinsaw0class@gmail.com>
2024-02-21 21:32:26 +0545

    layout for login and registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
             $table->string('utype')->default('USR');
+           
+            
 
             $table->rememberToken();
             $table->timestamps();
